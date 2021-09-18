dc027f34421305789b3eb2a3c92b239958ba9d4b
jierlich <jierlich@umich.edu>
2021-09-17 21:58:32 -0400

    [CLEAN] style suggestions

--- contracts/ERC721FixedPricePurchase.sol
+++ contracts/ERC721FixedPricePurchase.sol
@@ -1,48 +1,51 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity >=0.8.4;
+pragma solidity =0.8.4;
 
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 
+
 contract ERC721FixedPricePurchase {
     /// @dev mapping from ERC721 address -> token id -> listing price
-    mapping(address => mapping(uint256 => uint256)) listing;
+    mapping(address => mapping(uint256 => uint256)) public listing;
 
     event Listed(
-        address indexed ERC721,
+        address indexed erc721,
         uint256 indexed tokenId,
         address indexed owner,
         uint256 price
     );
+
     event Delisted(
-        address indexed ERC721,
+        address indexed erc721,
         uint256 indexed tokenId,
-         address indexed owner
+        address indexed owner
     );
-    event Purchased(address indexed ERC721,
+
+    event Purchased(address indexed erc721,
         uint256 indexed tokenId,
         address indexed buyer
     );
 
     // TODO: only owner can list
-    function list(address ERC721, uint256 tokenId, uint256 price) public {
-        listing[ERC721][tokenId] = price;
-        emit Listed(ERC721, tokenId, msg.sender, price);
+    function list(address erc721, uint256 tokenId, uint256 price) public {
+        listing[erc721][tokenId] = price;
+        emit Listed(erc721, tokenId, msg.sender, price);
     }
 
     // TODO: make sure approval removed beforehand, only owner can list
-    function delist(address ERC721, uint256 tokenId) public {
-        listing[ERC721][tokenId] = 0;
-        emit Delisted(ERC721, tokenId, msg.sender);
+    function delist(address erc721, uint256 tokenId) public {
+        listing[erc721][tokenId] = 0;
+        emit Delisted(erc721, tokenId, msg.sender);
     }
 
-    function purchase(address ERC721, uint256 tokenId) public payable {
-        require(msg.value >= listing[ERC721][tokenId], "ERC721FixedPricePurchase: Buyer didn't send enough ether");
-        listing[ERC721][tokenId] = 0;
-        address from = IERC721(ERC721).ownerOf(tokenId);
-        IERC721(ERC721).safeTransferFrom(from, msg.sender, tokenId);
+    function purchase(address erc721, uint256 tokenId) public payable {
+        require(msg.value >= listing[erc721][tokenId], "ERC721FixedPricePurchase: Buyer didn't send enough ether");
+        listing[erc721][tokenId] = 0;
+        address from = IERC721(erc721).ownerOf(tokenId);
+        IERC721(erc721).safeTransferFrom(from, msg.sender, tokenId);
         (bool sent, bytes memory data) = from.call{value: msg.value}("");
         require(sent, "ERC721FixedPricePurchase: Failed to send Ether");
-        emit Purchased(ERC721, tokenId, msg.sender);
+        emit Purchased(erc721, tokenId, msg.sender);
     }
 }
