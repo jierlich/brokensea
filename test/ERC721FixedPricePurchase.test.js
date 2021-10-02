830de0fad17cf44380e22ee6088de7dd4d2140a7
jierlich <jierlich@umich.edu>
2021-10-01 20:59:04 -0400

    [TEST] block sale of unlisted NFT

--- test/ERC721FixedPricePurchase.test.js
+++ test/ERC721FixedPricePurchase.test.js
@@ -52,6 +52,24 @@ describe("ERC721FixedPricePurchase", async () => {
         expect(curBal1).to.equal(prevBal1.add(ethers.utils.parseEther("0.01")))
         expect(curBal2).to.equal(prevBal2.sub(gasCost).sub(ethers.utils.parseEther("0.01")))
     })
+
+    it('blocks the sale of unlisted nfts', async () => {
+        // This test is specifically concerned with the case where the purchase contract is approved to transfer the
+        // ERC721 before a listing price is registered
+        await this.MockERC721
+            .connect(this.signers[1])
+            .approve(this.ERC721FixedPricePurchase.address, BN(1))
+
+        await expect(
+            this.ERC721FixedPricePurchase
+                .connect(this.signers[2])
+                .purchase(
+                    this.MockERC721.address,
+                    BN(1),
+                    {value: ethers.utils.parseEther("0")}
+                )
+        ).to.be.revertedWith('ERC721FixedPricePurchase: Token is not listed')
+    })
 })
 
 function getBalance(signer) {
