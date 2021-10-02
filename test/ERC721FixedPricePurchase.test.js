3dfb56ca792bed295a33b93d8fd645ac205bbf33
jierlich <jierlich@umich.edu>
2021-10-01 21:20:28 -0400

    [TEST] delisted NFTs

--- test/ERC721FixedPricePurchase.test.js
+++ test/ERC721FixedPricePurchase.test.js
@@ -66,7 +66,7 @@ describe("ERC721FixedPricePurchase", async () => {
                 .purchase(
                     this.MockERC721.address,
                     BN(1),
-                    {value: ethers.utils.parseEther("0")}
+                    {value: 0}
                 )
         ).to.be.revertedWith('ERC721FixedPricePurchase: Token is not listed')
     })
@@ -112,6 +112,50 @@ describe("ERC721FixedPricePurchase", async () => {
                     this.MockERC721.address,
                     BN(1),
                     {value: ethers.utils.parseEther("0.01")}
+                )
+        ).to.be.revertedWith('ERC721FixedPricePurchase: Token is not listed')
+    })
+
+    it('blocks the sale of delisted nfts', async () => {
+        // Part A: delisting by removing approval
+        await this.MockERC721
+            .connect(this.signers[1])
+            .approve(this.ERC721FixedPricePurchase.address, BN(1))
+        await this.ERC721FixedPricePurchase
+            .connect(this.signers[1])
+            .list(this.MockERC721.address, BN(1), ethers.utils.parseEther("0.01"))
+        await this.MockERC721
+            .connect(this.signers[1])
+            .approve(ethers.constants.AddressZero, BN(1))
+
+        await expect(
+            this.ERC721FixedPricePurchase
+                .connect(this.signers[2])
+                .purchase(
+                    this.MockERC721.address,
+                    BN(1),
+                    {value: ethers.utils.parseEther("0.01")}
+                )
+        ).to.be.revertedWith('ERC721: transfer caller is not owner nor approved')
+
+        // Part B: delisting by setting price to 0
+        await this.ERC721FixedPricePurchase
+            .connect(this.signers[1])
+            .list(this.MockERC721.address, BN(1), ethers.utils.parseEther("0.01"))
+        await this.MockERC721
+            .connect(this.signers[1])
+            .approve(this.ERC721FixedPricePurchase.address, BN(1))
+        await this.ERC721FixedPricePurchase
+            .connect(this.signers[1])
+            .list(this.MockERC721.address, BN(1), ethers.utils.parseEther("0"))
+
+        await expect(
+            this.ERC721FixedPricePurchase
+                .connect(this.signers[2])
+                .purchase(
+                    this.MockERC721.address,
+                    BN(1),
+                    {value: 0}
                 )
         ).to.be.revertedWith('ERC721FixedPricePurchase: Token is not listed')
     })
