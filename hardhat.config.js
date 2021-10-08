03bbbcd6d2527c0f98b9bf379e25eff1880e2c84
jierlich <jierlich@umich.edu>
2021-10-07 23:18:45 -0400

    [DEV] add ropsten and etherscan to hardhat

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,10 +1,22 @@
 require("@nomiclabs/hardhat-waffle");
 require("@nomiclabs/hardhat-etherscan");
+const environment = require('./env')
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {},
+    ropsten: {
+      url: `https://eth-ropsten.alchemyapi.io/v2/${environment.alchemyAPIKey}`,
+      accounts: [environment.deployerPK]
+    }
+  },
+  etherscan: {
+    apiKey: environment.etherscanAPIKey
+  },
   solidity: {
     version: "0.8.4",
     optimizer: {
